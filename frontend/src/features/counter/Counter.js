12834e2a0c0b62ccc9763df119b9e1881806081d
VIVEK YADAV <yadav7738vivek@gmail.com>
2024-04-27 13:48:21 +0530

    firts

---
+++ frontend/src/features/counter/Counter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+
+export default function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
