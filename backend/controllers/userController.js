394f5a6e2fd45ce59e19aa626295f2f4117a78b7
VIVEK YADAV <yadav7738vivek@gmail.com>
2024-06-27 23:52:32 +0530

    login and register backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,16 +37,25 @@ exports.loginUser = catchAsyncErrors(async(req,res,next)=>{
     if(!email || !password){
         return next(new ErrorHander("Please Enter email and password",400));
     }
-    const user = User.findOne({email}).select("+password");
+    const user = await User.findOne({email}).select("+password");
 
     if(!user){
-        return next(new ErrorHander("Invalid email or password"));
+        return next(new ErrorHander("Invalid email or password",401));
     }
 
-    const isPasswordMatched = user.comparePassword();
+    const isPasswordMatched = user.comparePassword(password);
     if(!isPasswordMatched){
-        return next(new ErrorHander("Invalid email or password"));
+        return next(new ErrorHander("Invalid email or password",401));
     }
+
+    const token = user.getJWTToken();
+
+
+    res.status(200).json({
+        success:true,
+        token,
+
+    });
 })
 
 
