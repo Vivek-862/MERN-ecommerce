6e0171384175262095e5038551b7ec21c965cf7c
VIVEK YADAV <yadav7738vivek@gmail.com>
2024-07-04 23:49:22 +0530

    email duplicate error backend done

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -12,6 +12,25 @@ module.exports = (err,req,res,next)=>{
         err= new ErrorHandler(message,400);
     }
 
+
+    //mongoose duplicate key error
+    if(err.code === 11000){
+        const message =`Duplicate ${Object.keys(err.keyValue)} Entered` 
+        err =  new ErrorHandler(message, 400);
+    }
+
+    //wrong JWT err
+    if(err.name ==="TokenExpiredError"){
+        const message = `Json Web Token is Expired,try again`;
+        err= new ErrorHandler(message,400);
+    }
+    
+    //JWT EXPIRE ERROR
+    if(err.name ==="JsonWebTokenError"){
+        const message = `Json Web Token is invalid,try again`;
+        err= new ErrorHandler(message,400);
+    }
+
     res.status(err.statusCode).json({
         success:false,
         message:err.message,
